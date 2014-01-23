f84edc4c1ecb0945dd14bfe3e2313d99ff2c030a
Jens Breitbart <jbreitbart@gmail.com>
2014-01-23 15:47:23 +0100

    Made python files GPL so it can easily be bundled with GPI2.

--- python/pyIBV.py
+++ python/pyIBV.py
@@ -1,4 +1,17 @@
 # Copyright (c) University of Heidelberg - Jens Breitbart <jens.breitbart@iwr.uni-heidelberg.de>, 2014
+# This file is part of GPI-2.
+
+# GPI-2 is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# version 3 as published by the Free Software Foundation.
+
+# GPI-2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GPI-2. If not, see <http://www.gnu.org/licenses/>.
 
 __docformat__ =  'restructuredtext'
 
