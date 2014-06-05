ae432e8ab6702ba858b6538bbdbe1c7d883ff0c9
Rui Machado <rui.machado@itwm.fhg.de>
2014-06-05 14:30:48 +0200

    Add small testcase for sn port change

---
+++ tests/tests/config/sn_port.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <test_utils.h>
+
+int main(int argc, char *argv[])
+{
+  gaspi_config_t default_conf;
+
+  TSUITE_INIT(argc, argv);
+
+  ASSERT (gaspi_config_get(&default_conf));
+
+  default_conf.sn_port = 21212;
+
+  ASSERT (gaspi_config_set(default_conf));
+
+
+  ASSERT (gaspi_proc_init(GASPI_BLOCK));
+  
+  ASSERT (gaspi_proc_term(GASPI_BLOCK));
+
+  return EXIT_SUCCESS;
+}
