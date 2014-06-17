c6734c8bf21a12caa30146eca43b420646f180c8
LenaO <lena.oden@itwm.fhg.de>
2014-06-17 09:54:18 -0700

    add GPU tests, modify gpu tests

--- tests/tests/gpu/seg_gpu_register.c
+++ tests/tests/gpu/seg_gpu_register.c
@@ -3,9 +3,7 @@
 #include <GASPI_GPU.h>
 #include <test_utils.h>
 #include <cuda.h> 
-//alloc max number of segs, fail after that
-//and them register them all with all nodes
-//then delete them
+//create shared gpu segment
 int main(int argc, char *argv[])
 {
   TSUITE_INIT(argc, argv);
@@ -17,30 +15,23 @@ int main(int argc, char *argv[])
 
   gaspi_rank_t rank, nprocs, i;
   gaspi_number_t seg_max;
+  gaspi_gpu_t gpus[8]; 
+  gaspi_gpu_num nGPUs;
 
   ASSERT(gaspi_proc_num(&nprocs));
   ASSERT (gaspi_proc_rank(&rank));
 
   ASSERT(gaspi_init_GPUs());
-  cudaSetDevice(0);
-  seg_max = 1;
-  
- // return 0;
+  ASSERT (gaspi_number_of_GPUs(&nGPUs));
+  ASSERT (gaspi_GPU_ids(gpus));
+
+
+  cudaSetDevice(gpus[0]);
+
   ASSERT (gaspi_segment_create(0, 1024,GASPI_GROUP_ALL,GASPI_BLOCK, GASPI_MEM_INITIALIZED|GASPI_MEM_GPU ));
 
 
   ASSERT(gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
-  
-  for (i = 0; i < nprocs; i++)
-    {
-
-      
-      if(i == rank)
-	continue;
-
-      ASSERT( gaspi_segment_register(0, i, GASPI_BLOCK));
-      //      sleep(1);
-    }
 
   ASSERT(gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
 
