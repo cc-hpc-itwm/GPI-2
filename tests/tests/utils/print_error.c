6cfcb8933a679fe842a039e83ca03657425833ff
Rui Machado <rui.machado@itwm.fhg.de>
2015-05-13 17:34:55 +0200

    Update small test to print all error numbers.

--- tests/tests/utils/print_error.c
+++ tests/tests/utils/print_error.c
@@ -11,14 +11,22 @@ int main(int argc, char *argv[])
   gaspi_return_t ret;
 
   ret = gaspi_proc_rank(&rank);
-  gaspi_printf("Err %d - %s\n", ret, gaspi_error_str(ret));
+  gaspi_printf_to(rank, "Err %2d - %s\n", ret, gaspi_error_str(ret));
   TSUITE_INIT(argc, argv);
 
   ret = gaspi_proc_init(GASPI_BLOCK);
-  gaspi_printf("Err %d - %s\n", ret, gaspi_error_str(ret));
+  gaspi_proc_rank(&rank);
   
+  gaspi_printf_to(rank, "Err %2d - %s\n", ret, gaspi_error_str(ret));
+
+  int err;
+  for(err = -1; err < 30; err++)
+      gaspi_printf_to(rank, "Err %2d - %s\n", err, gaspi_error_str(err));
+
+  gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK);
+
   ret = gaspi_proc_term(GASPI_BLOCK);
-  gaspi_printf("Err %d - %s\n", ret, gaspi_error_str(ret)); 
+  gaspi_printf_to(rank, "Err %2d - %s\n", ret, gaspi_error_str(ret));
 
   return EXIT_SUCCESS;
 }
