6b8a53bf91900f61fa1f1133945305d5b501bfa3
Rui Machado <rui.machado@itwm.fhg.de>
2015-07-07 09:10:36 +0200

    Update segment reuse test for more stress.

--- tests/tests/segments/seg_reuse.c
+++ tests/tests/segments/seg_reuse.c
@@ -6,6 +6,8 @@
 //alloc a segment, delete it and re-create it
 int main(int argc, char *argv[])
 {
+  int ntimes = 10;
+
   gaspi_rank_t rank, nprocs;
   
   TSUITE_INIT(argc, argv);
@@ -15,21 +17,24 @@ int main(int argc, char *argv[])
   ASSERT(gaspi_proc_num(&nprocs));
   ASSERT (gaspi_proc_rank(&rank));
 
-  ASSERT (gaspi_segment_create(0, 1024, 
-			       GASPI_GROUP_ALL, 
-			       GASPI_BLOCK, 
-			       GASPI_MEM_UNINITIALIZED));
+  do
+    {
+      ASSERT (gaspi_segment_create(0, 1024,
+				   GASPI_GROUP_ALL,
+				   GASPI_BLOCK,
+				   GASPI_MEM_UNINITIALIZED));
 
-  ASSERT (gaspi_segment_delete(0));
+      ASSERT (gaspi_segment_delete(0));
 
-  ASSERT(gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
-  
-  ASSERT (gaspi_segment_create(0, 2048,
-  			       GASPI_GROUP_ALL,
-  			       GASPI_BLOCK,
-  			       GASPI_MEM_UNINITIALIZED));
+      ASSERT (gaspi_segment_create(0, 2048,
+				   GASPI_GROUP_ALL,
+				   GASPI_BLOCK,
+				   GASPI_MEM_UNINITIALIZED));
 
-  ASSERT (gaspi_segment_delete(0));
+      ASSERT (gaspi_segment_delete(0));
+      ntimes--;
+    }
+  while(ntimes > 0);
 
   ASSERT(gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
 
