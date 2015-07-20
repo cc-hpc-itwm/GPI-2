0c1a251d3253082f2eac84e04fedf2e0cad544d1
Rui Machado <rui.machado@itwm.fhg.de>
2015-07-20 13:41:39 +0200

    Reduce test's required resources.

--- tests/tests/comm/write_all_nsizes.c
+++ tests/tests/comm/write_all_nsizes.c
@@ -20,32 +20,30 @@ int main(int argc, char *argv[])
   ASSERT (gaspi_proc_num(&numranks));
   ASSERT (gaspi_proc_rank(&myrank));
 
-  ASSERT (gaspi_segment_create(0, _2GB, GASPI_GROUP_ALL, GASPI_BLOCK, GASPI_MEM_INITIALIZED));
+  ASSERT (gaspi_segment_create(0, _8MB, GASPI_GROUP_ALL, GASPI_BLOCK, GASPI_MEM_INITIALIZED));
 
-  ASSERT( gaspi_segment_size(0, myrank, &segSize));
-
-  gaspi_printf("seg size %lu MB \n", segSize/1024/1024);
-
-  //  ASSERT (gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
+  ASSERT (gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
 
   ASSERT (gaspi_queue_size_max(&qmax));
 
-  for(commSize= 1; commSize < _500MB; commSize*=2 )
-    for(rankSend = 0; rankSend < numranks; rankSend++)
-      {
-	gaspi_printf("rank to send: %d - %lu bytes\n", rankSend, commSize);
-	
-	gaspi_queue_size(1, &queueSize);
-	if (queueSize > qmax - 24)
-  	  ASSERT (gaspi_wait(1, GASPI_BLOCK));
-	
-      ASSERT (gaspi_write(0, localOff, rankSend, 0,  remOff,  commSize, 1, GASPI_BLOCK));
-      
-      }
+  for(commSize= 1; commSize <= _8MB; commSize*=2 )
+    {
+      for(rankSend = 0; rankSend < numranks; rankSend++)
+	{
+	  gaspi_printf("rank to send: %d - %lu bytes\n", rankSend, commSize);
+
+	  gaspi_queue_size(1, &queueSize);
+	  if (queueSize > qmax - 24)
+	    ASSERT (gaspi_wait(1, GASPI_BLOCK));
+
+	  ASSERT (gaspi_write(0, localOff, rankSend, 0,  remOff,  commSize, 1, GASPI_BLOCK));
+	}
+    }
+
   ASSERT (gaspi_wait(1, GASPI_BLOCK));
-  
+
   ASSERT (gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
-  
+
   ASSERT (gaspi_proc_term(GASPI_BLOCK));
 
   return EXIT_SUCCESS;
