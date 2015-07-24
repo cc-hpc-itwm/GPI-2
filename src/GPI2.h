d39ab64e589cc9ac8c6502dda24e2458e3b82d44
Rui Machado <rui.machado@itwm.fhg.de>
2015-07-24 16:09:13 +0200

    Remove unused variable.

--- src/GPI2.h
+++ src/GPI2.h
@@ -32,11 +32,11 @@ along with GPI-2. If not, see <http://www.gnu.org/licenses/>.
 gaspi_context glb_gaspi_ctx;
 gaspi_group_ctx glb_gaspi_group_ctx[GASPI_MAX_GROUPS];
 
+/* TODO: Move these to context */
 volatile int glb_gaspi_init;         //Is GPI-2 initialized?
 volatile int glb_gaspi_sn_init;      //Is the SN up?
 volatile int glb_gaspi_dev_init;     //Is device initialized?
 volatile int gaspi_master_topo_data; //Do we have topology info from master?
-volatile int gaspi_rrcd_data;        //??
 
 //locks
 gaspi_lock_t glb_gaspi_ctx_lock;
