21ab928ffbb8dae5e47f469e5c43137df7fbbff7
Rui Machado <rui.machado@itwm.fhg.de>
2016-01-15 17:08:22 +0100

    Add extra types to fortran binding.

--- src/GASPI_Ext.f90
+++ src/GASPI_Ext.f90
@@ -15,63 +15,80 @@
 ! You should have received a copy of the GNU General Public License
 ! along with GPI-2. If not, see <http://www.gnu.org/licenses/>.
 
-!-----------------------------------------------------------------------
-module GASPI_Ext
+module GASPI_ext_types
 !-----------------------------------------------------------------------
 
   use, intrinsic :: ISO_C_BINDING
   implicit none
 
-    interface !gaspi_segment_bind
-       function gaspi_segment_bind ( segment_id                  &
-            &                      , memory_description          &
-            &                      , pointer                     &
-            &                      , size                        &
-            &                      )                             &
-            & result (res) bind (C, name="gaspi_segment_bind")
-         integer (gaspi_segment_id_t), value :: segment_id
-         integer (gaspi_memory_description_t), value :: memory_description
-         type (c_ptr), value :: pointer
-         integer (gaspi_size_t), value :: size
-         integer (gaspi_return_t) :: res
-       end function gaspi_segment_bind
-    end interface
+  integer, parameter   :: gaspi_memory_description_t = c_int
 
-    interface !gaspi_segment_use       
-       function gaspi_segment_use ( segment_id                  &
-            &                     , memory_description          &
-            &                     , pointer                     &
-            &                     , size                        &
-            &                     , group                       &
-            &                     , timeout                     &
-            &                     )                             &
-            & result (res) bind (C, name="gaspi_segment_use")
-         integer (gaspi_segment_id_t), value :: segment_id
-         integer (gaspi_memory_description_t), value :: memory_description
-         type (c_ptr), value :: pointer
-         integer (gaspi_size_t), value :: size
-         integer (gaspi_group_t), value :: group
-         integer (gaspi_timeout_t), value :: timeout
-         integer (gaspi_return_t) :: res
-       end function gaspi_segment_use
-    end interface
+end module GASPI_ext_types
 
-    interface !gaspi_queue_create
-       function gaspi_queue_create ( queue    &
-                                   , timeout) &
-            & result(res) bind (C, name="gaspi_queue_create" )
-         integer(gaspi_queue_id_t) :: queue
-         integer(gaspi_timeout_t), value :: timeout
-         integer(gaspi_return_t) :: res
-       end function gaspi_queue_create
-    end interface
-    
-    interface !gaspi_queue_delete
-       function gaspi_queue_delete ( queue ) &
-            & result(res) bind (C, name="gaspi_queue_delete" )
-         integer(gaspi_queue_id_t), value :: queue
-         integer(gaspi_return_t) :: res
-       end function gaspi_queue_delete
-    end interface
-    
+!-----------------------------------------------------------------------
+module GASPI_Ext
+!-----------------------------------------------------------------------
+
+  use, intrinsic :: ISO_C_BINDING
+  use GASPI_types
+  use GASPI_ext_types
+
+  implicit none
+
+  interface !gaspi_segment_bind
+     function gaspi_segment_bind ( segment_id                  &
+	  &                      , memory_description          &
+	  &                      , pointer                     &
+	  &                      , size                        &
+	  &                      )                             &
+	  & result (res) bind (C, name="gaspi_segment_bind")
+       import
+       integer (gaspi_segment_id_t), value :: segment_id
+       integer (gaspi_memory_description_t), value :: memory_description
+       type (c_ptr), value :: pointer
+       integer (gaspi_size_t), value :: size
+       integer (gaspi_return_t) :: res
+     end function gaspi_segment_bind
+  end interface
+
+  interface !gaspi_segment_use
+     function gaspi_segment_use ( segment_id                  &
+	  &                     , memory_description          &
+	  &                     , pointer                     &
+	  &                     , size                        &
+	  &                     , group                       &
+	  &                     , timeout                     &
+	  &                     )                             &
+	  & result (res) bind (C, name="gaspi_segment_use")
+       import
+       integer (gaspi_segment_id_t), value :: segment_id
+       integer (gaspi_memory_description_t), value :: memory_description
+       type (c_ptr), value :: pointer
+       integer (gaspi_size_t), value :: size
+       integer (gaspi_group_t), value :: group
+       integer (gaspi_timeout_t), value :: timeout
+       integer (gaspi_return_t) :: res
+     end function gaspi_segment_use
+  end interface
+
+  interface !gaspi_queue_create
+     function gaspi_queue_create ( queue    &
+	  , timeout) &
+	  & result(res) bind (C, name="gaspi_queue_create" )
+       import
+       integer(gaspi_queue_id_t) :: queue
+       integer(gaspi_timeout_t), value :: timeout
+       integer(gaspi_return_t) :: res
+     end function gaspi_queue_create
+  end interface
+
+  interface !gaspi_queue_delete
+     function gaspi_queue_delete ( queue ) &
+	  & result(res) bind (C, name="gaspi_queue_delete" )
+       import
+       integer(gaspi_queue_id_t), value :: queue
+       integer(gaspi_return_t) :: res
+     end function gaspi_queue_delete
+  end interface
+
 end module GASPI_Ext
