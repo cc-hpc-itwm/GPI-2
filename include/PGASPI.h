4d71a3c0399cce43592fbbd5e7bc19d987fcdb91
Rui Machado <rui.machado@itwm.fhg.de>
2016-03-30 17:06:37 +0200

    Add new functionality (queues, segments) to strong symbols header.

--- include/PGASPI.h
+++ include/PGASPI.h
@@ -26,7 +26,6 @@ extern "C"
 
 #include "GASPI.h"
 
- 
   gaspi_return_t pgaspi_config_get (gaspi_config_t * const config);
   
   gaspi_return_t pgaspi_config_set (const gaspi_config_t new_config);
@@ -48,13 +47,13 @@ extern "C"
   gaspi_return_t pgaspi_proc_num (gaspi_rank_t * const proc_num);
 
   gaspi_return_t pgaspi_proc_kill (const gaspi_rank_t rank,
-				  const gaspi_timeout_t timeout_ms);
+				   const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_connect (const gaspi_rank_t rank,
-				const gaspi_timeout_t timeout_ms);
+				 const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_disconnect (const gaspi_rank_t rank,
-				   const gaspi_timeout_t timeout_ms);
+				    const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_group_create (gaspi_group_t * const group);
 
@@ -62,56 +61,75 @@ extern "C"
   gaspi_return_t pgaspi_group_delete (const gaspi_group_t group);
 
   gaspi_return_t pgaspi_group_add (const gaspi_group_t group,
-				  const gaspi_rank_t rank);
+				   const gaspi_rank_t rank);
 
   gaspi_return_t pgaspi_group_commit (const gaspi_group_t group,
-				     const gaspi_timeout_t timeout_ms);
+				      const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_group_num (gaspi_number_t * const group_num);
 
   gaspi_return_t pgaspi_group_size (const gaspi_group_t group,
-				   gaspi_number_t * const group_size);
+				    gaspi_number_t * const group_size);
 
   gaspi_return_t pgaspi_group_ranks (const gaspi_group_t group,
-				    gaspi_rank_t * const group_ranks);
+				     gaspi_rank_t * const group_ranks);
 
   gaspi_return_t pgaspi_group_max (gaspi_number_t * const group_max);
 
   gaspi_return_t pgaspi_segment_alloc (const gaspi_segment_id_t segment_id,
-				      const gaspi_size_t size,
-				      const gaspi_alloc_t alloc_policy);
+				       const gaspi_size_t size,
+				       const gaspi_alloc_t alloc_policy);
 
   gaspi_return_t pgaspi_segment_delete (const gaspi_segment_id_t segment_id);
 
 
   gaspi_return_t pgaspi_segment_register (const gaspi_segment_id_t segment_id,
-					 const gaspi_rank_t rank,
-					 const gaspi_timeout_t timeout_ms);
+					  const gaspi_rank_t rank,
+					  const gaspi_timeout_t timeout_ms);
 
 
   gaspi_return_t pgaspi_segment_create (const gaspi_segment_id_t segment_id,
-				       const gaspi_size_t size,
-				       const gaspi_group_t group,
-				       const gaspi_timeout_t timeout_ms,
-				       const gaspi_alloc_t alloc_policy);
+					const gaspi_size_t size,
+					const gaspi_group_t group,
+					const gaspi_timeout_t timeout_ms,
+					const gaspi_alloc_t alloc_policy);
+  gaspi_return_t pgaspi_segment_bind ( gaspi_segment_id_t const segment_id,
+				       gaspi_pointer_t const pointer,
+				       gaspi_size_t const size,
+				       gaspi_memory_description_t const memory_description);
+
+  gaspi_return_t pgaspi_segment_use ( gaspi_segment_id_t const segment_id,
+				      gaspi_pointer_t const pointer,
+				      gaspi_size_t const size,
+				      gaspi_group_t const group,
+				      gaspi_timeout_t const timeout,
+				      gaspi_memory_description_t const memory_description);
 
   gaspi_return_t pgaspi_segment_num (gaspi_number_t * const segment_num);
 
   gaspi_return_t pgaspi_segment_list (const gaspi_number_t num,
-				     gaspi_segment_id_t * const segment_id_list);
+				      gaspi_segment_id_t * const segment_id_list);
 
   gaspi_return_t pgaspi_segment_ptr (const gaspi_segment_id_t segment_id,
-				    gaspi_pointer_t * ptr);
+				     gaspi_pointer_t * ptr);
 
 
   gaspi_return_t pgaspi_segment_size (const gaspi_segment_id_t segment_id,
-				     const gaspi_rank_t rank,
-				     gaspi_size_t * const size);
+				      const gaspi_rank_t rank,
+				      gaspi_size_t * const size);
 
 
   gaspi_return_t pgaspi_segment_max (gaspi_number_t * const segment_max);
 
   gaspi_return_t pgaspi_write (const gaspi_segment_id_t segment_id_local,
+			       const gaspi_offset_t offset_local,
+			       const gaspi_rank_t rank,
+			       const gaspi_segment_id_t segment_id_remote,
+			       const gaspi_offset_t offset_remote,
+			       const gaspi_size_t size,
+			       const gaspi_queue_id_t queue,
+			       const gaspi_timeout_t timeout_ms);
+  gaspi_return_t pgaspi_read (const gaspi_segment_id_t segment_id_local,
 			      const gaspi_offset_t offset_local,
 			      const gaspi_rank_t rank,
 			      const gaspi_segment_id_t segment_id_remote,
@@ -119,18 +137,21 @@ extern "C"
 			      const gaspi_size_t size,
 			      const gaspi_queue_id_t queue,
 			      const gaspi_timeout_t timeout_ms);
-  gaspi_return_t pgaspi_read (const gaspi_segment_id_t segment_id_local,
-			     const gaspi_offset_t offset_local,
-			     const gaspi_rank_t rank,
-			     const gaspi_segment_id_t segment_id_remote,
-			     const gaspi_offset_t offset_remote,
-			     const gaspi_size_t size,
-			     const gaspi_queue_id_t queue,
-			     const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_write_list (const gaspi_number_t num,
-				   gaspi_segment_id_t *
-				   const segment_id_local,
+				    gaspi_segment_id_t *
+				    const segment_id_local,
+				    gaspi_offset_t * const offset_local,
+				    const gaspi_rank_t rank,
+				    gaspi_segment_id_t *
+				    const segment_id_remote,
+				    gaspi_offset_t * const offset_remote,
+				    gaspi_size_t * const size,
+				    const gaspi_queue_id_t queue,
+				    const gaspi_timeout_t timeout_ms);
+
+  gaspi_return_t pgaspi_read_list (const gaspi_number_t num,
+				   gaspi_segment_id_t * const segment_id_local,
 				   gaspi_offset_t * const offset_local,
 				   const gaspi_rank_t rank,
 				   gaspi_segment_id_t *
@@ -139,147 +160,139 @@ extern "C"
 				   gaspi_size_t * const size,
 				   const gaspi_queue_id_t queue,
 				   const gaspi_timeout_t timeout_ms);
-
-  gaspi_return_t pgaspi_read_list (const gaspi_number_t num,
-				  gaspi_segment_id_t * const segment_id_local,
-				  gaspi_offset_t * const offset_local,
-				  const gaspi_rank_t rank,
-				  gaspi_segment_id_t *
-				  const segment_id_remote,
-				  gaspi_offset_t * const offset_remote,
-				  gaspi_size_t * const size,
-				  const gaspi_queue_id_t queue,
-				  const gaspi_timeout_t timeout_ms);
   gaspi_return_t pgaspi_wait (const gaspi_queue_id_t queue,
-			     const gaspi_timeout_t timeout_ms);
+			      const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_barrier (const gaspi_group_t group,
-				const gaspi_timeout_t timeout_ms);
+				 const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_allreduce (const gaspi_pointer_t buffer_send,
-				  gaspi_pointer_t const buffer_receive,
-				  const gaspi_number_t num,
-				  const gaspi_operation_t operation,
-				  const gaspi_datatype_t datatyp,
-				  const gaspi_group_t group,
-				  const gaspi_timeout_t timeout_ms);
+				   gaspi_pointer_t const buffer_receive,
+				   const gaspi_number_t num,
+				   const gaspi_operation_t operation,
+				   const gaspi_datatype_t datatyp,
+				   const gaspi_group_t group,
+				   const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_allreduce_user (const gaspi_pointer_t const buffer_send,
-				       gaspi_pointer_t const buffer_receive,
-				       const gaspi_number_t num,
-				       const gaspi_size_t element_size,
-				       gaspi_reduce_operation_t const
-				       reduce_operation,
-				       gaspi_state_t const reduce_state,
-				       const gaspi_group_t group,
-				       const gaspi_timeout_t timeout_ms);
+					gaspi_pointer_t const buffer_receive,
+					const gaspi_number_t num,
+					const gaspi_size_t element_size,
+					gaspi_reduce_operation_t const
+					reduce_operation,
+					gaspi_state_t const reduce_state,
+					const gaspi_group_t group,
+					const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_atomic_fetch_add (const gaspi_segment_id_t segment_id,
-					 const gaspi_offset_t offset,
-					 const gaspi_rank_t rank,
-					 const gaspi_atomic_value_t val_add,
-					 gaspi_atomic_value_t * const val_old,
-					 const gaspi_timeout_t timeout_ms);
+					  const gaspi_offset_t offset,
+					  const gaspi_rank_t rank,
+					  const gaspi_atomic_value_t val_add,
+					  gaspi_atomic_value_t * const val_old,
+					  const gaspi_timeout_t timeout_ms);
   gaspi_return_t pgaspi_atomic_compare_swap (const gaspi_segment_id_t
-					    segment_id,
-					    const gaspi_offset_t offset,
-					    const gaspi_rank_t rank,
-					    const gaspi_atomic_value_t
-					    comparator,
-					    const gaspi_atomic_value_t
-					    val_new,
-					    gaspi_atomic_value_t *
-					    const val_old,
-					    const gaspi_timeout_t timeout_ms);
+					     segment_id,
+					     const gaspi_offset_t offset,
+					     const gaspi_rank_t rank,
+					     const gaspi_atomic_value_t
+					     comparator,
+					     const gaspi_atomic_value_t
+					     val_new,
+					     gaspi_atomic_value_t *
+					     const val_old,
+					     const gaspi_timeout_t timeout_ms);
   gaspi_return_t pgaspi_passive_send (const gaspi_segment_id_t
-				     segment_id_local,
-				     const gaspi_offset_t offset_local,
-				     const gaspi_rank_t rank,
-				     const gaspi_size_t size,
-				     const gaspi_timeout_t timeout_ms);
+				      segment_id_local,
+				      const gaspi_offset_t offset_local,
+				      const gaspi_rank_t rank,
+				      const gaspi_size_t size,
+				      const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_passive_receive (const gaspi_segment_id_t
-					segment_id_local,
-					const gaspi_offset_t offset_local,
-					gaspi_rank_t * const rem_rank,
-					const gaspi_size_t size,
-					const gaspi_timeout_t timeout_ms);
+					 segment_id_local,
+					 const gaspi_offset_t offset_local,
+					 gaspi_rank_t * const rem_rank,
+					 const gaspi_size_t size,
+					 const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_notify (const gaspi_segment_id_t segment_id_remote,
-			       const gaspi_rank_t rank,
-			       const gaspi_notification_id_t notification_id,
-			       const gaspi_notification_t notification_value,
-			       const gaspi_queue_id_t queue,
-			       const gaspi_timeout_t timeout_ms);
+				const gaspi_rank_t rank,
+				const gaspi_notification_id_t notification_id,
+				const gaspi_notification_t notification_value,
+				const gaspi_queue_id_t queue,
+				const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_notify_waitsome (const gaspi_segment_id_t
-					segment_id_local,
-					const gaspi_notification_id_t
-					notification_begin,
-					const gaspi_number_t num,
-					gaspi_notification_id_t *
-					const first_id,
-					const gaspi_timeout_t timeout_ms);
+					 segment_id_local,
+					 const gaspi_notification_id_t
+					 notification_begin,
+					 const gaspi_number_t num,
+					 gaspi_notification_id_t *
+					 const first_id,
+					 const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_notify_reset (const gaspi_segment_id_t
-				     segment_id_local,
-				     const gaspi_notification_id_t
-				     notification_id,
-				     gaspi_notification_t *
-				     const old_notification_val);
+				      segment_id_local,
+				      const gaspi_notification_id_t
+				      notification_id,
+				      gaspi_notification_t *
+				      const old_notification_val);
 
   gaspi_return_t pgaspi_write_notify (const gaspi_segment_id_t
-				     segment_id_local,
-				     const gaspi_offset_t offset_local,
-				     const gaspi_rank_t rank,
-				     const gaspi_segment_id_t
-				     segment_id_remote,
-				     const gaspi_offset_t offset_remote,
-				     const gaspi_size_t size,
-				     const gaspi_notification_id_t
-				     notification_id,
-				     const gaspi_notification_t
-				     notification_value,
-				     const gaspi_queue_id_t queue,
-				     const gaspi_timeout_t timeout_ms);
+				      segment_id_local,
+				      const gaspi_offset_t offset_local,
+				      const gaspi_rank_t rank,
+				      const gaspi_segment_id_t
+				      segment_id_remote,
+				      const gaspi_offset_t offset_remote,
+				      const gaspi_size_t size,
+				      const gaspi_notification_id_t
+				      notification_id,
+				      const gaspi_notification_t
+				      notification_value,
+				      const gaspi_queue_id_t queue,
+				      const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_write_list_notify (const gaspi_number_t num,
-					  gaspi_segment_id_t *
-					  const segment_id_local,
-					  gaspi_offset_t * const offset_local,
-					  const gaspi_rank_t rank,
-					  gaspi_segment_id_t *
-					  const segment_id_remote,
-					  gaspi_offset_t *
-					  const offset_remote,
-					  gaspi_size_t * const size,
-					  const gaspi_segment_id_t
-					  segment_id_notification,
-					  const gaspi_notification_id_t
-					  notification_id,
-					  const gaspi_notification_t
-					  notification_value,
-					  const gaspi_queue_id_t queue,
-					  const gaspi_timeout_t timeout_ms);
+					   gaspi_segment_id_t *
+					   const segment_id_local,
+					   gaspi_offset_t * const offset_local,
+					   const gaspi_rank_t rank,
+					   gaspi_segment_id_t *
+					   const segment_id_remote,
+					   gaspi_offset_t *
+					   const offset_remote,
+					   gaspi_size_t * const size,
+					   const gaspi_segment_id_t
+					   segment_id_notification,
+					   const gaspi_notification_id_t
+					   notification_id,
+					   const gaspi_notification_t
+					   notification_value,
+					   const gaspi_queue_id_t queue,
+					   const gaspi_timeout_t timeout_ms);
 
   gaspi_return_t pgaspi_queue_size (const gaspi_queue_id_t queue,
-				   gaspi_number_t * const queue_size);
+				    gaspi_number_t * const queue_size);
 
   gaspi_return_t pgaspi_queue_num (gaspi_number_t * const queue_num);
+
+  gaspi_return_t pgaspi_queue_create(gaspi_queue_id_t * const queue_id,
+				     const gaspi_timeout_t timeout_ms);
+
+  gaspi_return_t pgaspi_queue_delete(const gaspi_queue_id_t queue_id);
 
   gaspi_return_t pgaspi_queue_size_max (gaspi_number_t * const queue_size_max);
 
   gaspi_return_t pgaspi_transfer_size_min (gaspi_size_t *
-					  const transfer_size_min);
+					   const transfer_size_min);
 
   gaspi_return_t pgaspi_transfer_size_max (gaspi_size_t *
-					  const transfer_size_max);
+					   const transfer_size_max);
 
   gaspi_return_t pgaspi_notification_num (gaspi_number_t * const notification_num);
 
-  gaspi_return_t pgaspi_passive_transfer_size_max (gaspi_size_t *
-						  const
-						  passive_transfer_size_max);
+  gaspi_return_t pgaspi_passive_transfer_size_max (gaspi_size_t * const passive_transfer_size_max);
 
   gaspi_return_t pgaspi_allreduce_buf_size (gaspi_size_t * const buf_size);
 
@@ -316,17 +329,17 @@ extern "C"
 
   gaspi_return_t
   pgaspi_statistic_counter_info(gaspi_statistic_counter_t counter
-			       , gaspi_statistic_argument_t* counter_argument
-			       , gaspi_string_t* counter_name
-			       , gaspi_string_t* counter_description
-			       , gaspi_number_t* verbosity_level
-			       );
+				, gaspi_statistic_argument_t* counter_argument
+				, gaspi_string_t* counter_name
+				, gaspi_string_t* counter_description
+				, gaspi_number_t* verbosity_level
+				);
   
   gaspi_return_t
   pgaspi_statistic_counter_get ( gaspi_statistic_counter_t counter
-				, gaspi_number_t argument
-				, unsigned long *value
-				);
+				 , gaspi_number_t argument
+				 , unsigned long *value
+				 );
 
   gaspi_return_t pgaspi_statistic_counter_reset (gaspi_statistic_counter_t counter);
 
