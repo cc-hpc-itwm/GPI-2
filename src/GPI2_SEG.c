c77e8310c008ead5bc8115be59b5f4479f69c7c9
Rui Machado <rui.machado@itwm.fhg.de>
2016-04-28 17:50:35 +0200

    Fix indentation.

--- src/GPI2_SEG.c
+++ src/GPI2_SEG.c
@@ -493,9 +493,9 @@ pgaspi_segment_bind ( gaspi_segment_id_t const segment_id,
 #pragma weak gaspi_segment_use = pgaspi_segment_use
 gaspi_return_t
 pgaspi_segment_use ( gaspi_segment_id_t const segment_id,
-		    gaspi_pointer_t const pointer,
-		    gaspi_size_t const size,
-		    gaspi_group_t const group,
+		     gaspi_pointer_t const pointer,
+		     gaspi_size_t const size,
+		     gaspi_group_t const group,
 		     gaspi_timeout_t const timeout,
 		     gaspi_memory_description_t const memory_description)
 {
