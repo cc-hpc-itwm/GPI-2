b66ec55ed918263f4b9e414545a122927239b86d
Rui Machado <rui.machado@itwm.fhg.de>
2016-04-28 17:46:57 +0200

    Move constants away from interface.

--- include/GASPI.h
+++ include/GASPI.h
@@ -34,23 +34,6 @@ extern "C"
 {
 #endif
 
-  /* Macro-ed constants */
-#define GASPI_MAJOR_VERSION (1)
-#define GASPI_MINOR_VERSION (3)
-#define GASPI_REVISION (0)
-
-#define GASPI_MAX_NODES   (65536)
-#define GASPI_MAX_GROUPS  (32)
-#define GASPI_MAX_MSEGS   (32)
-#define GASPI_MAX_QP      (16)
-#define GASPI_COLL_QP     (GASPI_MAX_QP)
-#define GASPI_PASSIVE_QP  (GASPI_MAX_QP+1)
-#define GASPI_SN          (GASPI_MAX_QP+2)
-#define GASPI_MAX_TSIZE_C ((1ul<<31ul)-1ul)
-#define GASPI_MAX_TSIZE_P ((1ul<<16ul)-1ul)
-#define GASPI_MAX_QSIZE   (4096)
-#define GASPI_MAX_NOTIFICATION  (65536)
-
   /* Types */
   typedef char gaspi_char;
   typedef unsigned char gaspi_uchar;
