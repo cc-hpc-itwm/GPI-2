0d45190e3f59b7360850d5f77cde49590135b026
Rui Machado <rui.machado@itwm.fhg.de>
2016-09-08 14:11:09 +0200

    Clean test.

--- tests/tests/segments/seg_create_same.c
+++ tests/tests/segments/seg_create_same.c
@@ -5,23 +5,13 @@
 
 int main(int argc, char *argv[])
 {
-  gaspi_rank_t rank, nprocs;
-  
   TSUITE_INIT(argc, argv);
 
   ASSERT (gaspi_proc_init(GASPI_BLOCK));
 
-  //need the barrier to make sn is up
-  ASSERT(gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
-
-  ASSERT(gaspi_proc_num(&nprocs));
-  ASSERT (gaspi_proc_rank(&rank));
-
   ASSERT (gaspi_segment_create(0, _2MB, GASPI_GROUP_ALL, GASPI_BLOCK,GASPI_MEM_UNINITIALIZED));
   ASSERT (gaspi_segment_create(0, _8MB, GASPI_GROUP_ALL, GASPI_BLOCK,GASPI_MEM_UNINITIALIZED));
   
-  ASSERT(gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
-
   ASSERT(gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
 
   ASSERT (gaspi_proc_term(GASPI_BLOCK));
