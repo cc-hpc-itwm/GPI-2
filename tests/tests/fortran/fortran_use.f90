a6d335e352d52f29f4e72da6d21da71e7a87f68b
Rui Machado <rui.machado@itwm.fhg.de>
2016-11-23 13:07:27 +0100

    Update test (fortran_use).

--- tests/tests/fortran/fortran_use.f90
+++ tests/tests/fortran/fortran_use.f90
@@ -4,17 +4,18 @@
 
 program main
   use gaspi
+  use gaspi_ext
   use , intrinsic :: ISO_C_BINDING
 
   integer(gaspi_size_t) :: transfer, bufsize
   integer(gaspi_number_t) :: num
   integer(gaspi_return_t) :: ret
   integer(gaspi_queue_id_t) :: q
-  integer(gaspi_rank_t) :: rank, nprocs, remoteRank
+  integer(gaspi_rank_t) :: rank, nprocs, remoteRank, local_rank, local_num
   character(LEN=100) :: out_str
   character(LEN=32) :: istr
   integer(gaspi_segment_id_t) :: seg_id
-  integer(gaspi_size_t) :: seg_size, arr_size
+  integer(gaspi_size_t) :: seg_size, arr_sizem, my_seg_size
   integer(gaspi_alloc_t) :: seg_alloc
   integer(gaspi_notification_t) :: notf, recv_val
   integer(gaspi_notification_id_t) :: notf_id, start, recv_notf
@@ -27,6 +28,7 @@ program main
   integer(gaspi_offset_t), target :: list_remoffs(255)
   integer(gaspi_size_t), target :: list_sizes(255)
   integer(gaspi_atomic_value_t) :: atom_value
+  integer(gaspi_number_t) :: initialized
   type(c_ptr) :: segs_ptr, locoff_ptr, remoff_ptr, sizes_ptr
   type(c_ptr) :: seg_ptr
   integer :: seg_int_size, recvmsgs, i
@@ -60,9 +62,8 @@ program main
   ret = gaspi_queue_size(q, num)
   write(*,*) "Queue size: ", num
 
-
-  write(*,*) "GASPI_BLOCK", GASPI_BLOCK
-  write(*,*) "GASPI_TEST", GASPI_TEST
+  ret = gaspi_initialized(initialized)
+  write(*,*) "Is initialized(before): ", initialized
 
   !init
   ret = gaspi_proc_init(GASPI_BLOCK)
@@ -71,19 +72,29 @@ program main
      call exit(-1)
   end if
 
+  ret = gaspi_initialized(initialized)
+  write(*,*) "Is initialized(after): ", initialized
+
   ret = gaspi_proc_rank(rank)
   ret = gaspi_proc_num(nprocs)
 
+  ret = gaspi_proc_local_rank(local_rank)
+  ret = gaspi_proc_local_num(local_num)
+
+  write(*,*) "Rank :", rank," of ", nprocs, " Local: ", local_rank," of ", local_num
+
   seg_id = 0
-  seg_size = 4 * 1024 * 1024 * 1024
-  seg_int_size = 4 * 1024 * 1024 * 1024
+  seg_size = 2 * 1024 * 1024
+  seg_int_size = 2 * 1024 * 1024
 
   seg_alloc = GASPI_MEM_UNINITIALIZED
-  ret = gaspi_segment_create(INT(0,1),seg_size ,GASPI_GROUP_ALL, GASPI_BLOCK, seg_alloc) !GASPI_MEM_INITIALIZED)
+  ret = gaspi_segment_create(INT(0,1), seg_size, GASPI_GROUP_ALL, GASPI_BLOCK, seg_alloc) !GASPI_MEM_INITIALIZED)
   if(ret .ne. GASPI_SUCCESS) then
      write(*,*) "gaspi_create_segment failed"
      call exit(-1)
   end if
+
+  ret = gaspi_segment_size(INT(0,1), rank, my_seg_size)
 
   ret = gaspi_segment_ptr(INT(0,1), seg_ptr)
   if(ret .ne. GASPI_SUCCESS) then
@@ -106,32 +117,25 @@ program main
   end if
 
   !communication
+  ! write to right neighboor
   remoteRank = modulo(rank + 1, nprocs)
   localOff = (rank * sizeof(rank))
   remoteOff = (rank * sizeof(rank))
   queue = 0
   tsize = sizeof(rank)
 
+  !write at the beginning of segment
   ret = gaspi_write(seg_id, localOff, remoteRank, &
-       & seg_id, remoteOff,tsize, queue, GASPI_BLOCK)
+       & seg_id, remoteOff, tsize, queue, GASPI_BLOCK)
   if(ret .ne. GASPI_SUCCESS) then
-     write(*,*) "gaspi_write faile1d"
-     call exit(-1)
-  end if
-
-  localOff = seg_size - sizeof(rank) - (rank * sizeof(rank))
-  remoteOff = seg_size - sizeof(rank) - (rank * sizeof(rank))
-
-  ret = gaspi_write(seg_id, localOff, remoteRank, &
-       & seg_id, remoteOff,tsize, queue, GASPI_BLOCK)
-  if(ret .ne. GASPI_SUCCESS) then
-     write(*,*) "gaspi_write failed"
+     write(*,*) "first gaspi_write failed"
      call exit(-1)
   end if
 
   localOff = (seg_size / 2 ) + (rank * sizeof(rank))
-  remoteOff = (seg_size / 2 ) + (rank * sizeof(rank))
+  remoteOff = localOff
 
+  !write in the middle of segment
   ret = gaspi_write(seg_id, localOff, remoteRank, &
        & seg_id, remoteOff, tsize, queue, GASPI_BLOCK)
   if(ret .ne. GASPI_SUCCESS) then
@@ -139,14 +143,13 @@ program main
      call exit(-1)
   end if
 
-  !wait
   ret = gaspi_wait(queue, GASPI_BLOCK)
   if(ret .ne. GASPI_SUCCESS) then
      write(*,*) "gaspi_wait failed"
      call exit(-1)
   end if
 
-  !notifications
+  !notify partner rank
   notf = 70000
   start = 45678
   recv_notf = 0
@@ -184,12 +187,18 @@ program main
   end if
 
   !check data
-  do i = 1, seg_size/sizeof(rank)
-     if( arr(i) .ne. remoteRank) then
-        write(*,*) "data is wrong"
-        call exit(-1)
-     end if
-  end do
+  i = remoteRank+1
+  if( arr(i) .ne. remoteRank) then
+     write(*,*) "Rank ", rank, " :data is wrong at ", i, " is ", arr(i), " should be ", remoteRank
+     call exit(-1)
+  end if
+
+  i = seg_size / 2 / sizeof(rank) + remoteRank + 1
+  if( arr(i) .ne. remoteRank) then
+     write(*,*) "Rank ", rank, " : middle data is wrong at ", i, " is ", arr(i), " should be ", remoteRank
+     call exit(-1)
+  end if
+
   !passive
   localOff = 0
   tsize = 8
@@ -291,10 +300,11 @@ program main
 	write(*,*) "gaspi_atomic_fetch_add failed"
 	call exit(-1)
      end if
-     if(atom_value .eq. nprocs) then
-	write(*,*), "Correct atomic value: ", atom_value
+     if(atom_value .ne. nprocs) then
+	write(*,*), "Incorrect atomic value: ", atom_value
      end if
   end if
+
   !term
   ret = gaspi_proc_term(GASPI_BLOCK)
   if(ret .ne. GASPI_SUCCESS) then
