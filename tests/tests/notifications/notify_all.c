5ec3c95e8377bed4e6f6f70a4e483840aebe7295
Rui Machado <rui.machado@itwm.fhg.de>
2017-02-09 14:49:55 +0100

    tests: cosmetics: remove unncessary printf.

--- tests/tests/notifications/notify_all.c
+++ tests/tests/notifications/notify_all.c
@@ -7,9 +7,9 @@
 int main(int argc, char *argv[])
 {
   TSUITE_INIT(argc, argv);
-  
+
   ASSERT (gaspi_proc_init(GASPI_BLOCK));
-  
+
   gaspi_notification_id_t notif = 0;
   gaspi_number_t notif_num, n;
   gaspi_rank_t rank, nprocs, i;
@@ -17,16 +17,15 @@ int main(int argc, char *argv[])
 
   ASSERT(gaspi_proc_num(&nprocs));
   ASSERT (gaspi_proc_rank(&rank));
-  
+
   ASSERT (gaspi_segment_create(seg_id, 1024, GASPI_GROUP_ALL, GASPI_BLOCK, GASPI_MEM_UNINITIALIZED));
-  
+
   ASSERT( gaspi_notification_num(&notif_num));
-  gaspi_printf("max num notifications %u\n", notif_num);
-  
+
   gaspi_number_t queue_size;
   gaspi_number_t queue_max;
   ASSERT (gaspi_queue_size_max(&queue_max));
-  
+
   for(n = 0; n < notif_num; n++)
     {
       for(i = 0; i < nprocs; i++)
@@ -46,7 +45,7 @@ int main(int argc, char *argv[])
     {
       gaspi_notification_id_t id;
       ASSERT (gaspi_notify_waitsome(seg_id, 0, notif_num, &id, GASPI_BLOCK));
-      
+
       gaspi_notification_t notification_val;
       ASSERT( gaspi_notify_reset(seg_id, id, &notification_val));
       assert( notification_val == rank + 1 );
@@ -56,6 +55,6 @@ int main(int argc, char *argv[])
   ASSERT(gaspi_wait(0, GASPI_BLOCK));
   ASSERT (gaspi_barrier(GASPI_GROUP_ALL, GASPI_BLOCK));
   ASSERT (gaspi_proc_term(GASPI_BLOCK));
-  
+
   return EXIT_SUCCESS;
 }
