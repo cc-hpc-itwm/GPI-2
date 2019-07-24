3f575cba9d1d654950facf978e8cbbefec724eea
Rui Machado <rui.machado@itwm.fhg.de>
2019-07-24 16:21:55 +0200

    tcp: include missing license header in files.

--- src/devices/tcp/rb.c
+++ src/devices/tcp/rb.c
@@ -1,3 +1,20 @@
+/*
+Copyright (c) Fraunhofer ITWM - Carsten Lojewski <lojewski@itwm.fhg.de>, 2013-2019
+
+This file is part of GPI-2.
+
+GPI-2 is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+version 3 as published by the Free Software Foundation.
+
+GPI-2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GPI-2. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <pthread.h>
 
 #include "rb.h"
