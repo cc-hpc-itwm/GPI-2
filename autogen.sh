d1fba99c641b5479b2b9d3027dab53bce2da11f9
Arcesio Castaneda Medina <acastanedam@gmail.com>
2019-09-12 22:29:15 +0200

    Add conditionally subdir's automake option
    This avoids warning messages coming from more recent automake versions
    and possible future incompatibilities.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 add=$1
 
+# SUBDIR OPTION FOR MODERN AUTOMAKE
+atmk_ver=`automake --version | head -n 1 | awk '{print $NF}'`
+if [[ `echo ${atmk_ver%.*} \>= 1.14 | bc` -eq 1 ]]; then
+    sed 's/AUTOMAKE_SUBDIRS/subdir-objects/g' configure.ac.in > configure.ac
+else
+    sed 's/AUTOMAKE_SUBDIRS//g' configure.ac.in > configure.ac
+fi
+
+# AUTOTOOLS CONFIGURATION
 if [[ ! -e build-aux ]]; then
     mkdir build-aux
 fi
