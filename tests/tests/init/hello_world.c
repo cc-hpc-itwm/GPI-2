b136f8f2e69fc962b644e25108089ca88fb69576
Arcesio Castaneda Medina <arcesio.castaneda.medina@itwm.fraunhofer.de>
2020-07-24 16:02:47 +0200

    tests/tests/init:clean/add missing init tests
    *) Add hello_world
    *) Remove exit_failure and proc_init

--- tests/tests/init/hello_world.c
+++ tests/tests/init/hello_world.c
@@ -5,13 +5,9 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <test_utils.h>
+
 int main(int argc, char *argv[])
 {
-  gaspi_config_t conf;
-  ASSERT(gaspi_config_get(&conf));
-  conf.mtu = 2048;
-  conf.build_infrastructure = 0;
-  ASSERT(gaspi_config_set(conf));
   TSUITE_INIT(argc, argv);
 
   ASSERT (gaspi_proc_init(GASPI_BLOCK));
@@ -19,9 +15,8 @@ int main(int argc, char *argv[])
 
   ASSERT (gaspi_proc_rank(&rank));
   ASSERT (gaspi_proc_num(&num));
-  
-  gaspi_printf("Hello from rank %d of %d -> %d\n", 
-	       rank, num, (rank + 1 ) % num );
+
+  gaspi_printf ("Hello from rank %d of %d\n", rank, num);
 
   ASSERT (gaspi_proc_term(GASPI_BLOCK));
 
