8feb136cebe0960a44d2e0c3af03e9f9550eb161
Rui Machado <rui.machado@itwm.fhg.de>
2021-03-19 13:08:11 +0100

    meta: update copyright date.

--- src/GPI2_IO.c
+++ src/GPI2_IO.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) Fraunhofer ITWM - Carsten Lojewski <lojewski@itwm.fhg.de>, 2013-2020
+  Copyright (c) Fraunhofer ITWM - Carsten Lojewski <lojewski@itwm.fhg.de>, 2013-2021
 
   This file is part of GPI-2.
 
