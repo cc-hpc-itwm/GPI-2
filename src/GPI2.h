7905bcc722a3696152264220b186a80aa56ad8c7
Arcesio Castaneda Medina <arcesio.castaneda.medina@itwm.fraunhofer.de>
2021-11-23 22:08:18 +0100

    fix_notif_space_size: use GASPI_MAX_NOTIFICATION
    The original definition of NOTIFICATIONS_SPACE_SIZE uses directly the
    explicit number 65536 but not GASPI_MAX_NOTIFICATION

--- src/GPI2.h
+++ src/GPI2.h
@@ -31,7 +31,8 @@ along with GPI-2. If not, see <http://www.gnu.org/licenses/>.
    space. For the latter we use 64 though, to have a larger alignment
    of the data space (hacky!).
 */
-#define NOTIFICATIONS_SPACE_SIZE ((65536 * sizeof(gaspi_notification_t)) + (64))
+#define NOTIFICATIONS_SPACE_SIZE                                               \
+  ((GASPI_MAX_NOTIFICATION * sizeof(gaspi_notification_t)) + (64))
 
 extern gaspi_context_t glb_gaspi_ctx;
 
